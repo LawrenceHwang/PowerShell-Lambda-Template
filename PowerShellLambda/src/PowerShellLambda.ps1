8594d1719559c9a4455a51fa96168cb6720c2981
Lawrence Hwang <coffeepoweredlion@gmail.com>
2019-04-30 21:10:46 -0700

    Update doco a bit

--- PowerShellLambda/src/PowerShellLambda.ps1
+++ PowerShellLambda/src/PowerShellLambda.ps1
@@ -1,13 +1,21 @@
+<#
+By default in this template, we are including the AWSPowerShell.NetCore module
+when packing the PowerShell code.
+If you do not need AWSPowerShell.NetCore module, you can move following #Requires line into the comment block.
+#>
+
 #Requires -Modules 'AWSPowerShell.NetCore'
 
-#Outputting to CloudWatch for troubleshooting. Comment this section if you don't need it.
+# By default, outputting to the environment to CloudWatch for easier troubleshooting later on.
+# Comment this section if you don't need it.
 Write-Host '$LambdaInput'
 Write-Host -Object $LambdaInput
-$t1 = ConvertTo-Json -Compress $LambdaInput -Depth 5
-Write-Verbose -Message $t1 -Verbose
+$LambdaInputJson = ConvertTo-Json -Compress $LambdaInput -Depth 5
+Write-Verbose -Message $LambdaInputJson -Verbose
+
 Write-Host 'ENV'
-$t2 = Get-ChildItem env:\ | ConvertTo-Json -Depth 1
-Write-Verbose -Message $t2 -Verbose
+$ENVJson = Get-ChildItem env:\ | ConvertTo-Json -Depth 1
+Write-Verbose -Message $ENVJson -Verbose
 
 # Add your own code below. Have fun building. :)
 try
